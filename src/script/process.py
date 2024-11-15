eb74f892d44ef48d91b22a1974d80eacbd756398
MiYanDoris <dorisyan@pku.edu.cn>
2024-11-15 05:25:13 +0000

    allow command line json path

--- src/script/process.py
+++ src/script/process.py
@@ -39,27 +39,9 @@ def process_all(params):
 def main(cfg: DictConfig) -> None:
     assert '**' in cfg['data']['input_template'] and len(cfg['data']['input_template'].split('**')) == 2
 
-    # obj_lst = json.load(open('/mnt/afs/grasp-sim/yanmi/graspsim-new-pipeline/data/objaverse/annotation/objaverse-1400.json', 'r'))
+    obj_lst = json.load(open(cfg['object_json'], 'r'))
 
-    # obj_to_be_processed = []
-    # print('Go through object lists...')
-    # def judge_obj_to_be_processed(obj_id):
-    #     if not os.path.exists(cfg['task']['1-get_basic_info']['output_path'].replace('${data.output_template}', cfg['data']['output_template']).replace('**', obj_id)):
-    #         return [obj_id]
-    #     return []
-    
-    # pool = ThreadPoolExecutor(max_workers=32)
-    # result_iter = pool.map(judge_obj_to_be_processed, obj_lst)
-    # for result in result_iter:
-    #     obj_to_be_processed.extend(result)
-    
-    # with open('obj_to_be_processed.log', 'w') as f:
-    #     f.write('\n'.join(obj_to_be_processed))
-    # exit(0)
-
-    obj_to_be_processed = open('obj_to_be_processed.log', 'r').read().split('\n')
-    
-    obj_lst = obj_to_be_processed[cfg['split_id']::cfg['total_split_num']]
+    obj_lst = obj_lst[cfg['split_id']::cfg['total_split_num']]
 
     logger.info("#"*30)
     logger.info(f"Input template: {cfg['data']['input_template']}")
